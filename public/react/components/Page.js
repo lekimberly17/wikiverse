154b6a6552d5a6fe5ae47d0adad3b71f0892d52f
lekimberly17 <lekimberly509@gmail.com>
2023-03-19 23:11:25 -0700

    updates to the project for now

--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import apiURL from '../api';
 
-export const Page = (props) => {
+export const Page = ({ page, onBack, pages, setPages }) => {
+  const { title, content, createdAt, authorId, tags } = page;
+  const [user, setUser] = useState({});
 
-  return <>
-    <h3>{props.page.title}</h3>
-  </>
-} 
-	
+  async function fetchUser() {
+    try {
+      const response = await fetch(`${apiURL}/users/${authorId}`);
+      const userData = await response.json();
+      setUser(userData);
+    } catch (err) {
+      console.log("Error! ", err);
+    }
+  }
+
+  const { name } = user;
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  const deletePage = async () => {
+    const response = await fetch(`${apiURL}/wiki/${title}`, {
+      method: 'DELETE',
+    });
+    const data = await response.json();
+    console.log(data);
+
+    const updatedPages = pages.filter((p) => p.title !== title);
+    setPages(updatedPages);
+
+    onBack();
+  };
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>
+        <strong>Author</strong>: {name}
+      </p>
+      <p>
+        <strong>Published</strong>: {new Date(createdAt).toLocaleDateString()}
+      </p>
+      <p>
+        <strong>Article Content</strong>: {content}
+      </p>
+      <p>
+        <strong>Tags</strong>: {tags && tags.join(", ")}
+      </p>
+      <button onClick={deletePage}>DELETE</button>
+      <button onClick={onBack}>Back to Wiki List</button>
+    </div>
+  );
+};
--- public/react/components/PagesList.js
+++ public/react/components/PagesList.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Page } from './Page';
+import { AddPage } from './AddPage';
 
-export const PagesList = ({pages}) => {
-	return <>
-		{
-			pages.map((page, idx) => {
-				return <Page page={page} key={idx} />
-			})
-		}
-	</>
-} 
+export const PagesList = ({ pages, setPages }) => {
+  const [selectedPage, setSelectedPage] = useState(null);
+  const [showPage, setShowPage] = useState(false);
+
+  const handlePageClick = (page) => {
+    setSelectedPage(page);
+  };
+
+  const handleBackClick = () => {
+    setSelectedPage(null);
+  };
+
+  const handleCreatePageClick = () => {
+    setShowPage(true);
+  };
+
+  if (selectedPage) {
+    return (
+      <Page page={selectedPage} onBack={handleBackClick} pages={pages} setPages={setPages} />
+    );
+  }
+
+  return (
+    <div>
+      <h2>An interesting 📚</h2>
+      {pages.map((page) => (
+		<div key={page.title} onClick={() => handlePageClick(page)}>
+          		<h4>{page.title}</h4>
+				
+		</div>
+      ))}
+      {showPage ? (
+        <AddPage onArticleAdded={() => {setShowPage(false); setPages(pages)}} onClose={() => setShowPage(false)} />
+      ) : (
+        <button onClick={handleCreatePageClick}>Create a Page</button>
+      )}
+    </div>
+  );
+};
